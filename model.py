12a7ed0beaa7a15bb391c4a69f403168553b908a
evgenijdedov <evgggd@gmail.com>
2019-11-05 15:09:30 +0300

    add fully-connected gan

--- model.py
+++ model.py
@@ -41,6 +41,28 @@ class GeneratorCNN(nn.Module):
         return self.layers(x)
 
 
+class GeneratorFC(nn.Module):
+    def __init__(self, gan_noise_size, gan_output_size):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(gan_noise_size, 128),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.BatchNorm1d(128, momentum=0.99, eps=0.001),
+            nn.Linear(128, 2048),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.BatchNorm2d(32, momentum=0.99, eps=0.001),
+            nn.Linear(2048, 1024),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.BatchNorm2d(16, momentum=0.99, eps=0.001),
+            nn.Linear(1024, gan_output_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
 class DiscriminatorCNN(nn.Module):
     def __init__(self, gan_noise_size):
         super().__init__()
@@ -56,6 +78,29 @@ class DiscriminatorCNN(nn.Module):
             nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=0.2),
             Flatten(),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Dropout(0.2),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class DiscriminatorFC(nn.Module):
+    def __init__(self, gan_noise_size):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(gan_noise_size, 128),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Linear(gan_noise_size, 4096),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Linear(4096, 2048),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Linear(2048, 1024),
+            nn.LeakyReLU(negative_slope=0.2),
             nn.LeakyReLU(negative_slope=0.2),
             nn.Dropout(0.2),
             nn.Linear(1024, 1),
