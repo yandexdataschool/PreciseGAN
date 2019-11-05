3a4d5183d6297936d39681737559a27d1dfe49a4
evgenijdedov <evgggd@gmail.com>
2019-11-05 15:37:25 +0300

    use sgd

--- optim.py
+++ optim.py
@@ -10,7 +10,7 @@ def setup_optimizer(model, lr, weight_decay):
         {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
 
-    optimizer = Adam(optimizer_grouped_parameters, lr=lr)
-    # optimizer = SGD(optimizer_grouped_parameters, lr=lr)
+    # optimizer = Adam(optimizer_grouped_parameters, lr=lr)
+    optimizer = SGD(optimizer_grouped_parameters, lr=lr)
 
     return optimizer
