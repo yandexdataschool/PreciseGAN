d423a879c538f36a387aebbaba2a556b9e0cf1bd
thesinsay9 <nick.guschin1998@yandex.ru>
2019-11-07 21:25:46 +0300

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -3,7 +3,7 @@ import scipy.stats as stats
 import torch
 from matplotlib import pyplot as plt
 from tqdm import tqdm
-from data import PTCL_FEATURES
+from data import PTCL_FEATURES, DIJET_SYSTEM_FEATURES
 
 ANGLE_IDX = 5
 
@@ -31,7 +31,7 @@ def evaluate_model(generator, experiment, test_set, batch_size, batch_num, param
     inverse_generated = np.concatenate((inverse_generated, jj_M_gan), 1)
     test_set = np.concatenate((test_set, jj_M_test), 1)
 
-    fig, ax = plt.subplots(3, 3, figsize=(16, 8))
+    fig, ax = plt.subplots(3, 3, figsize=(20, 12))
 
     hist_bins = [20, 25, 30, 20, 25, 30, 15, 30, 20]
     hist_ranges = [(200, 800), (-2.5, 2.5), (0, 300), (200, 600), (-2.5, 2.5),
@@ -44,14 +44,13 @@ def evaluate_model(generator, experiment, test_set, batch_size, batch_num, param
 
     for i in range(inverse_generated.shape[1]):
         count_g, bin_widths_g = np.histogram(inverse_generated[:, i],
-                                             bins=hist_bins[i], range=hist_ranges[i], density=True)
+                                             bins=hist_bins[i], range=hist_ranges[i])
         count_t, bin_widths_t = np.histogram(test_set[:, i],
-                                             bins=hist_bins[i], range=hist_ranges[i], density=True)
+                                             bins=hist_bins[i], range=hist_ranges[i])
 
-        width = (max(bin_widths_t) - min(bin_widths_t)) / hist_bins[i]
 
-        ax[i // 3][i % 3].bar(bin_widths_g[:-1], count_g, width=width)
-        ax[i // 3][i % 3].bar(bin_widths_t[:-1], count_t, width=width, alpha=0.5)
+        ax[i // 3][i % 3].hist(inverse_generated[:, i], bins=hist_bins[i], range=hist_ranges[i], density = True)
+        ax[i // 3][i % 3].hist(test_set[:, i], bins=hist_bins[i], range=hist_ranges[i], alpha=0.5, density = True)
         ax[i // 3][i % 3].set_title(features[i])
         ax[i // 3][i % 3].set_ylabel('Events / Bin Width')
 
@@ -66,7 +65,7 @@ def evaluate_model(generator, experiment, test_set, batch_size, batch_num, param
         plt.text(0.9, 0.9, f'χ2/NDF: {round(chi2, 1)}', horizontalalignment='right',
                  verticalalignment='top', transform=ax[i // 3][i % 3].transAxes)
 
-        plt.text(0.9, 0.875, f'paper χ2/NDF: {paper_chi[i]}', horizontalalignment='right', fontdict={'color': 'red'},
+        plt.text(0.9, 0.8, f'paper χ2/NDF: {paper_chi[i]}', horizontalalignment='right', fontdict={'color': 'red'},
                  verticalalignment='top', transform=ax[i // 3][i % 3].transAxes)
 
     fig.title = f'architecture: {parametres.architecture}'
@@ -91,23 +90,23 @@ def evaluate_model(generator, experiment, test_set, batch_size, batch_num, param
          chi2_tail = stats.chisquare(count_t, count_g)[0] / (n_bins_chi - 1)
          chisqs_tail.append(chi2_tail)
 
-         ax[0].set_title('Linear hist')
+         ax[0].set_title('Linear hist didjet system m')
          ax[0].hist(jj_M_gan[:,2], bins=n_bins_chi, range= range_chi)
          ax[0].hist(jj_M_test[:,2], bins=n_bins_chi, range= range_chi, alpha=0.5)
-         ax[1].set_title('Log hist')
+         ax[1].set_title('Log hist didjet system m')
          ax[1].hist(jj_M_gan[:,2], bins=n_bins_chi, range=range_chi, log=True)
          ax[1].hist(jj_M_test[:,2], bins=n_bins_chi, range=range_chi, log=True, alpha=0.5)
 
-         plt.text(0.9, 0.9, round(chi2_tail, 1), horizontalalignment='right',
+         plt.text(0.9, 0.9,  f'χ2/NDF: {round(chi2_tail, 1)}', horizontalalignment='right',
                   verticalalignment='top', transform=ax[0].transAxes)
 
-         plt.text(0.9, 0.875, article_chi_tail, horizontalalignment='right', fontdict={'color': 'red'},
+         plt.text(0.9, 0.875, f'paper χ2/NDF: {article_chi_tail}', horizontalalignment='right', fontdict={'color': 'red'},
                   verticalalignment='top', transform=ax[0].transAxes)
 
-         plt.text(0.9, 0.9, round(chi2_tail, 1), horizontalalignment='right',
+         plt.text(0.9, 0.9, f'χ2/NDF: {round(chi2_tail, 1)}', horizontalalignment='right',
                   verticalalignment='top', transform=ax[1].transAxes)
 
-         plt.text(0.9, 0.875, article_chi_tail, horizontalalignment='right', fontdict={'color': 'red'},
+         plt.text(0.9, 0.875, f'paper χ2/NDF: {article_chi_tail}', horizontalalignment='right', fontdict={'color': 'red'},
                   verticalalignment='top', transform=ax[1].transAxes)
 
          fig_tail_chi.show()
@@ -124,16 +123,16 @@ def evaluate_model(generator, experiment, test_set, batch_size, batch_num, param
          ax[1].hist(jj_M_gan[:,2], bins=n_bins, range=range_hist, log=True)
          ax[1].hist(jj_M_test[:,2], bins=n_bins, range=range_hist, log=True, alpha=0.5)
 
-         plt.text(0.9, 0.9, round(chi2_tail, 1), horizontalalignment='right',
+         plt.text(0.9, 0.9, f'χ2/NDF: {round(chi2_tail, 1)}', horizontalalignment='right',
                   verticalalignment='top', transform=ax[0].transAxes)
 
-         plt.text(0.9, 0.875, article_chi_tail, horizontalalignment='right', fontdict={'color': 'red'},
+         plt.text(0.9, 0.875,  f'paper χ2/NDF: {article_chi_tail}', horizontalalignment='right', fontdict={'color': 'red'},
                   verticalalignment='top', transform=ax[0].transAxes)
 
-         plt.text(0.9, 0.9, round(chi2_tail, 1), horizontalalignment='right',
+         plt.text(0.9, 0.9, f'χ2/NDF: {round(chi2_tail, 1)}', horizontalalignment='right',
                   verticalalignment='top', transform=ax[1].transAxes)
 
-         plt.text(0.9, 0.875, article_chi_tail, horizontalalignment='right', fontdict={'color': 'red'},
+         plt.text(0.9, 0.875,  f'paper χ2/NDF: {article_chi_tail}', horizontalalignment='right', fontdict={'color': 'red'},
                   verticalalignment='top', transform=ax[1].transAxes)
 
          fig_tail.show()
